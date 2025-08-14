23320a4699378221f8d1fd052b37476fadce4a17
Muhammad Hasnain Fatmi <hasnainfatmi22@gmail.com>
2025-08-15 01:20:13 +0500

    Update Structure

--- github_integration_api/main.py
+++ github_integration_api/main.py
@@ -1,9 +1,9 @@
 import uvicorn
-from github_integration_api.src.server import app
+from src.server import app
 
 if __name__ == "__main__":
     uvicorn.run(
-        "github_integration_api.src.server:app",
+        "src.server:app",
         host="0.0.0.0",
         port=8000,
         reload=True
